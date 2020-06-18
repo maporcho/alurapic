dc2047a8c6d167a70a6d632aa9b006cf0a82f568
Marco Porcho <maporcho@gmail.com>
2020-06-18 17:29:45 -0300

    Aula 02 completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'alurapic';
+  photos = [
+    {
+      url:
+        'https://cdna.artstation.com/p/assets/images/images/008/865/742/large/borislav-mitkov-docbrowntest02.jpg',
+      description: 'Doc Brown',
+    },
+    {
+      url: 'https://images.uncyc.org/pt/d/df/Marty_67284_f_97180.jpg',
+      description: 'Marty McFly',
+    },
+    {
+      url: 'https://images.uncyc.org/pt/d/df/Marty_67284_f_97180.jpg',
+      description: 'Marty McFly',
+    },
+    {
+      url: 'https://images.uncyc.org/pt/d/df/Marty_67284_f_97180.jpg',
+      description: 'Marty McFly',
+    },
+  ];
 }
