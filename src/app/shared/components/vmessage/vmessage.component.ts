265eb9777710fd19caa85964b990222e6d471df5
Marco Porcho <maporcho@gmail.com>
2020-07-04 19:03:12 -0300

    Componentizando mensagens de validação

---
+++ src/app/shared/components/vmessage/vmessage.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'ap-vmessage',
+  templateUrl: './vmessage.component.html',
+})
+export class VmessageComponent {
+  @Input() text: string = '';
+}
