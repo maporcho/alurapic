9bb8448b0ba1601c2466b44af967417875131b69
Marco Porcho <maporcho@gmail.com>
2020-07-06 16:42:07 -0300

    Redirecionamento pós login

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -11,7 +12,8 @@ export class SigninComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -25,7 +27,10 @@ export class SigninComponent implements OnInit {
     const userName = this.loginForm.get('userName').value;
     const password = this.loginForm.get('password').value;
     this.authService.authenticate(userName, password).subscribe(
-      () => console.log('autenticado'),
+      () => {
+        console.log('autenticado');
+        this.router.navigate(['user/', userName]);
+      },
       (err) => {
         console.log(err);
         this.loginForm.reset();
