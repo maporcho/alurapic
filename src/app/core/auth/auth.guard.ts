47205c6ea2beab1ef3aeb93e2a7381c4d734cf65
Marco Porcho <maporcho@gmail.com>
2020-07-09 08:06:39 -0300

    Implementando guarda de rotas para usuário logado

---
+++ src/app/core/auth/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { UserService } from '../user/user.service';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | import('@angular/router').UrlTree
+    | import('rxjs').Observable<boolean | import('@angular/router').UrlTree>
+    | Promise<boolean | import('@angular/router').UrlTree> {
+    if (this.userService.isLogged()) {
+      this.router.navigate(['user', this.userService.getUserName()]);
+      return false;
+    }
+    return true;
+  }
+}
