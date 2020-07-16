581bb89f7bc4443c857ba2794005d5be88f8cefb
Marco Porcho <maporcho@gmail.com>
2020-07-16 19:42:16 -0300

    Apresentando preview da imagem selecionada

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { PhotoModule } from '../photo/photo.module';
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    VMessageModule,
+    RouterModule,
+    PhotoModule,
+  ],
 })
 export class PhotoFormModule {}
