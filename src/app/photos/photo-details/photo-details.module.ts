cbbebae00d8ecc525f4baa6efacd3d757b425ce4
Marco Porcho <maporcho@gmail.com>
2020-08-04 19:54:35 -0300

    Curtindo fotos

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.directive';
+import { ShowIfLoggedModule } from 'src/app/shared/directives/show-if-logged/show-if-logged.module';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.dir
     RouterModule,
     ReactiveFormsModule,
     VMessageModule,
+    ShowIfLoggedModule,
   ],
   declarations: [
     PhotoDetailsComponent,
