a41ff9fe831b89ec4a912b7bda8ddfc456b4469d
Marco Porcho <maporcho@gmail.com>
2020-08-10 20:02:22 -0300

    Indicador de progresso

---
+++ src/app/shared/components/loading/loading.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { LoadingService } from './loading.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private loadingService: LoadingService) {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      tap((event) => {
+        if (event instanceof HttpResponse) {
+          this.loadingService.stop();
+        } else {
+          this.loadingService.start();
+        }
+      })
+    );
+  }
+}
